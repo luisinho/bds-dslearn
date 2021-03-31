1915367816a234c14701373edd78d6a53f7ef0ea
luis3977@gmail.com <luis3977@gmail.com>
2021-03-31 18:51:59 -0300

    Observe design pattern, OCP SOLID principle

--- backend/src/main/java/com/devsuperior/dslearnbds/services/DeliverService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/DeliverService.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dslearnbds.services;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
 import com.devsuperior.dslearnbds.entities.Deliver;
+import com.devsuperior.dslearnbds.observers.DeliverRevisionObserver;
 import com.devsuperior.dslearnbds.repositories.DeliverRepository;
 
 @Service
@@ -14,6 +18,8 @@ public class DeliverService {
 	@Autowired
 	private DeliverRepository deliverRepository;
 
+	private Set<DeliverRevisionObserver> deliverRevisionObservers = new LinkedHashSet<DeliverRevisionObserver>();	
+
 	@Transactional
 	public void saveRevision(Long id, DeliverRevisionDTO dto) {
 
@@ -21,6 +27,14 @@ public class DeliverService {
 		deliver.setStatus(dto.getStatus());
 		deliver.setFeedback(dto.getFeedback());
 		deliver.setCorrectCount(dto.getCorrectCount());
-		this.deliverRepository.save(deliver);		
+		this.deliverRepository.save(deliver);
+
+		this.deliverRevisionObservers.forEach(observer -> {
+			observer.onSaveRevision(deliver);
+		});
+	}
+
+	public void subscribeDeliverRevisionObserver(DeliverRevisionObserver observer) {
+		this.deliverRevisionObservers.add(observer);
 	}
 }
--- backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dslearnbds.services;
 
+import java.time.Instant;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,18 +11,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.entities.Deliver;
 import com.devsuperior.dslearnbds.entities.Notification;
 import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.observers.DeliverRevisionObserver;
 import com.devsuperior.dslearnbds.repositories.NotificationRepository;
 
 @Service
-public class NotificationService {
+public class NotificationService implements DeliverRevisionObserver	{
 
 	@Autowired
 	private NotificationRepository notificationRepository;
 
 	@Autowired
 	private AuthService authService;
+
+	@Autowired
+	private DeliverService deliverService;
+
+	@PostConstruct
+	private void initialize() {
+		this.deliverService.subscribeDeliverRevisionObserver(this);
+	}
 
 	@Transactional(readOnly = true)
 	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pageable) {
@@ -29,4 +43,25 @@ public class NotificationService {
 
 		return page.map(x -> new NotificationDTO(x));
 	}
+
+	@Transactional
+	public void saveDeliverNotification(Deliver deliver) {
+
+		Long sectionId = deliver.getLesson().getSection().getId();
+		Long resourceId = deliver.getLesson().getSection().getResource().getId();
+		Long offerId = deliver.getLesson().getSection().getResource().getOffer().getId();
+
+		String route = "/offers/" + offerId + "/resources/" + resourceId + "/sections/" + sectionId;
+		String text = deliver.getFeedback();
+		Instant moment = Instant.now();
+		User user = deliver.getEnrollment().getStudent();
+
+		Notification notification = new Notification(null, text, moment, false, route, user);
+		this.notificationRepository.save(notification);
+	}
+
+	@Override
+	public void onSaveRevision(Deliver deliver) {
+		this.saveDeliverNotification(deliver);		
+	}	
 }
